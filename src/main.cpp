8be65b1fd3ff4b0ad362ec528190a2987be03355
Oliver Goroll <oliver.goroll@gmx.de>
2011-05-22 17:08:38 +0200

    new class Vec3d, moved main

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 	#include <GL/glut.h>
 #endif
 
-#include <iostream>
+//#include <iostream>
+#include <cmath>
+#include <limits>
+#include "util/vec3d.h"
+
+//double collision(Vec3d dPos, Vec3d dVel, double rx, double ry);
 
 using namespace std;
 
@@ -15,12 +20,12 @@ int timestart;
 
 void draw()
 {
-	cout << "Drawing." << endl;
+	//cout << "Drawing." << endl;
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	
 	double timediff = (double)glutGet(GLUT_ELAPSED_TIME) - timestart;
 	timestart = glutGet(GLUT_ELAPSED_TIME);
-	cout << "timediff: " << timediff << endl;
+	//cout << "timediff: " << timediff << endl;
 	double dist = speed * timediff;
 	
 	if(keys['a'])
@@ -33,29 +38,49 @@ void draw()
 		y -= dist;
 	
 	glLoadIdentity();
-	glTranslatef(x, y, 0.0);
-	glBegin(GL_TRIANGLES);
+	glPushMatrix();
+		glTranslatef(-1, 0, 0.0);
+		glutSolidSphere(0.25, 100, 100);
+	glPopMatrix();
+	
+	glTranslatef(1,0,0);
+	glutSolidSphere(0.25, 100, 100);
+	/*glBegin(GL_TRIANGLES);
 		glColor3f(1.0, 0.0, 0.0);
 		glVertex3f(-0.5,-0.5,0.0);
 		glColor3f(0.0, 1.0, 0.0);
 		glVertex3f(0.5,0.0,0.0);
 		glColor3f(0.0, 0.0, 1.0);
 		glVertex3f(0.0,0.5,0.0);
-	glEnd();
+	glEnd();*/
 	
 	glutSwapBuffers();
 }
 
 void keyPressed(unsigned char key, int, int)
 {
-	keys[key] = true;
+	//keys[key] = true;
 }
 
 
 void keyReleased(unsigned char key, int, int)
 {
-	keys[key] = false;
+	//keys[key] = false;
 }
+
+/*double collision(Vec3d dPos, Vec3d dVel, double rx, double ry, bool &coll)
+{
+	
+	double denom = 2 * (dPos.x*dVel.x + dPos.y*dVel.x);
+	if (abs(denom) <= numeric_limits<double>::epsilon()) {
+		coll = false;
+		return 0;
+	}
+	double nom = dPos.x*dPos.x + dPos.y*dPos.y + dPos.x*dVel.x + dPos.y*dVel.y - (rx + ry)*(rx + ry);
+	coll = true;
+	
+	return abs(nom / denum);
+}*/
 
 int main(int argc, char **argv)
 {
@@ -63,7 +88,7 @@ int main(int argc, char **argv)
 	timestart = glutGet(GLUT_ELAPSED_TIME);
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(800, 600);
+	glutInitWindowSize(600, 600);
 	glutCreateWindow("Soldat-Klon Prototyp, OpenGL-Test");
 	
 	glutDisplayFunc(draw);
---
+++ src/util/vec3d.h
@@ -0,0 +1,27 @@
+﻿#ifndef _VEC_H
+#define _VEC_H
+
+#include <cmath>
+
+class Vec3d
+{
+public:
+	double x, y, z;
+	
+	Vec3d():x(0.0),y(0.0),z(0.0)
+	{};
+	Vec3d(double _x, double _y, double _z):x(_x),y(_y),z(_z)
+	{};
+	
+	double length()
+	{
+		return sqrt(x*x + y*y + z*z);
+	};
+	
+	&Vec3d operator -(const Vec3d &a, const Vec3d &b)
+	{
+		return Vec3d(a.x - b.x, a.y - b.y, a.z - b.z);
+	};
+};
+
+#endif
